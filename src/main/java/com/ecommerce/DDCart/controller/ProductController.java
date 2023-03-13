b08e8ff31d8f68ab93ef6186b3174c2b055b0fcd
Deena0111 <deena011197@gmail.com>
2023-03-13 16:29:51 +0530

    product model created

--- src/main/java/com/ecommerce/DDCart/controller/ProductController.java
+++ src/main/java/com/ecommerce/DDCart/controller/ProductController.java
@@ -1,10 +1,18 @@
 package com.ecommerce.DDCart.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ecommerce.DDCart.model.Product;
 import com.ecommerce.DDCart.service.ProductService;
 
 @RestController
@@ -14,5 +22,18 @@ public class ProductController {
 	
 	@Autowired
 	ProductService productService;
+	
+	@PostMapping("/create")
+	public ResponseEntity<String> createCategory(@RequestBody Product product)
+	{
+		
+		return new ResponseEntity<String>(productService.saveCategory(product),HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/list")
+	public ResponseEntity<List<Product>> readAllCategory(){
+		return new ResponseEntity<List<Product>>(productService.listCategory(),HttpStatus.OK);
+	}
 
 }
--- src/main/java/com/ecommerce/DDCart/service/ProductService.java
+++ src/main/java/com/ecommerce/DDCart/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.ecommerce.DDCart.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ecommerce.DDCart.model.Product;
 import com.ecommerce.DDCart.repository.ProductRepository;
 
 @Service
@@ -11,6 +14,16 @@ public class ProductService {
 	@Autowired
 	ProductRepository productRepo;
 	
+	public String saveCategory(Product product)
+	{
+		productRepo.save(product);
+		return product.toString()+"  created sucessfully";
+	}
+
+	
+	public List<Product> listCategory() {
+		return productRepo.findAll();
+	}
 	
 
 }
